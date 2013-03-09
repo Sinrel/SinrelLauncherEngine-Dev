13e8722ac119df6db727bdd6cd221513ed007809
Alex Bond <alex.bondarevskiy@gmail.com>
2013-03-09 23:37:04 +0200

    Java 6 fix

--- src/org/sinrel/engine/actions/Checker.java
+++ src/org/sinrel/engine/actions/Checker.java
@@ -1,174 +1,154 @@
 package org.sinrel.engine.actions;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-
 import org.sinrel.engine.Engine;
 import org.sinrel.engine.library.NetManager;
 import org.sinrel.engine.library.OSManager;
 import org.sinrel.engine.library.cryption.MD5;
 import org.sinrel.engine.listeners.CheckerListener;
 
-final class Checker {
-	
-	//Проверяемые файлы. Так же проверяется наличие natives под систему клиента
-	static String[] files = { "jinput.jar" , "lwjgl.jar" , "lwjgl_util.jar" , "minecraft.jar" };
-	//static ArrayList<File> additionalFiles = new ArrayList<File>();
-	
-	static ArrayList<CheckerListener> listeners = new ArrayList<CheckerListener>();
-	
-	private Client clientStatus;
-	
-	Checker( String applicationName , String serverName  ) {
-		//FIXME Now do nothing :C
-	}
-	
-	Checker( String applicationName  ) {
-		for( CheckerListener cl : listeners  ) {
-			cl.onStartChecking();
-		}
-		
-		if( !NetManager.isOnline() ) {
-			clientStatus = Client.BAD_CONNECTION; 
-			return;
-		}
-		
-		if( !OSManager.getWorkingDirectory( applicationName ).exists() | !OSManager.getClientFolder( applicationName ).exists() ) {
-			clientStatus = Client.CLIENT_NOT_EXIST;
-			return;
-		}
-		
-		ArrayList<String> hash = new ArrayList<String>();
-		File f = null;
-		
-		for ( String name : files ) {
-			f = new File( OSManager.getClientFolder( applicationName ) , name );
-			if( !f.exists() ) {
-				clientStatus = Client.WRONG_CLIENT;
-				return;
-			}
-			
-			hash.add( MD5.getMD5( f ) );
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for( String s : hash ) {
-			sb.append( s );
-		}
-		
-		switch ( send( "standart", sb.toString() , OSManager.getPlatform() ) ) {
-			case BAD_CONNECTION:
-				onFinish();
-				clientStatus = Client.BAD_CONNECTION;
-				return;
-			
-			case CLIENT_DOES_NOT_MATCH:
-				onFinish();
-				clientStatus = Client.CLIENT_DOES_NOT_MATCH;
-				return;
-			
-			case CLIENT_NOT_EXIST:
-				onFinish();
-				clientStatus = Client.CLIENT_NOT_EXIST;
-				return;
-			
-			case CLIENT_NOT_EXIST_ON_SERVER:
-				onFinish();
-				clientStatus = Client.CLIENT_NOT_EXIST_ON_SERVER;
-				return;
-			
-			case OK:
-				onFinish();
-				clientStatus = Client.OK;
-				return;
-			
-			case WRONG_CLIENT:
-				onFinish();
-				clientStatus = Client.WRONG_CLIENT;
-				return;		
-		}
-	}
-	
-	Client getClientStatus() {
-		return clientStatus;
-	}
-	
-	private static Client send( String client, String hash , OSManager.OS system ) {
-	     try {	    	 
-	    	  StringBuilder data = new StringBuilder( URLEncoder.encode("action", "UTF-8") + "=" + URLEncoder.encode("check", "UTF-8") );
-	 		  
-	    	  data.append( "&" + URLEncoder.encode("client", "UTF-8") + "=" + URLEncoder.encode( client , "UTF-8" ) );
-	 		  data.append( "&" + URLEncoder.encode("hash", "UTF-8") + "=" + URLEncoder.encode( hash , "UTF-8") );
-	 		  data.append( "&" + URLEncoder.encode("system", "UTF-8") + "=" + URLEncoder.encode( system.toString() , "UTF-8") );
-	 		 
-	          URL url; 
-	          
-	          if( !Engine.getDescriptionFile().get("folder").equalsIgnoreCase("") ) {
-	        	  url =	new URL( 
-	        			  "http://" + Engine.getDescriptionFile().get("domain") + "/" +  Engine.getDescriptionFile().get("folder") + "/" + "engine.php" 
-	        	  );
-	          }else {
-	        	  url =	new URL( 
-	        			  "http://" + Engine.getDescriptionFile().get("domain") + "/" + "engine.php" 
-	        	  );
-	          }
-	          	
-	          URLConnection conn = url.openConnection();
-	          conn.setDoOutput(true);
-	          OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
-	          writer.write( data.toString() );
-	          writer.flush();
-	          
-	          StringBuffer s = new StringBuffer();
-	          BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	          String line;
-	          
-	          while ((line = reader.readLine()) != null) {
-	              s.append(line);
-	          }
-	          writer.close();
-	          reader.close();
-	          
-	          String answer = s.toString();	  
-	          	          
-	          switch ( answer ) {
-	          	case "BAD_CONNECTION" :
-	          		return Client.BAD_CONNECTION;
-	          	case "CLIENT_DOES_NOT_MATCH":
-	          		return Client.CLIENT_DOES_NOT_MATCH;
-	          	
-	          	case "CLIENT_NOT_EXITST":
-	          		return Client.CLIENT_NOT_EXIST;
-	          	
-	          	case "CLIENT_NOT_EXIST_ON_SERVER":
-	          		return Client.CLIENT_NOT_EXIST_ON_SERVER;
-	          	
-	          	case "WRONG_CLIENT":
-	          		return Client.WRONG_CLIENT;
-	          	
-	          	case "OK":
-	          		return Client.OK;
-	          }
-	          
-	     }catch (IOException e) {
-	 		return Client.BAD_CONNECTION;
-	     }
-	     
-	     return Client.BAD_CONNECTION;
-	}
-	
-	private static void onFinish() {
-		for( CheckerListener cl : listeners  ) {
-			cl.onFinishChecking();
-		}
-	}
-	
+final class Checker {
+
+    //Проверяемые файлы. Так же проверяется наличие natives под систему клиента
+    static String[] files = {"jinput.jar", "lwjgl.jar", "lwjgl_util.jar", "minecraft.jar"};
+    //static ArrayList<File> additionalFiles = new ArrayList<File>();
+
+    static ArrayList<CheckerListener> listeners = new ArrayList<CheckerListener>();
+
+    private Client clientStatus;
+
+    Checker(String applicationName, String serverName) {
+        //FIXME Now do nothing :C
+    }
+
+    Checker(String applicationName) {
+        for (CheckerListener cl : listeners) {
+            cl.onStartChecking();
+        }
+
+        if (!NetManager.isOnline()) {
+            clientStatus = Client.BAD_CONNECTION;
+            return;
+        }
+
+        if (!OSManager.getWorkingDirectory(applicationName).exists() | !OSManager.getClientFolder(applicationName).exists()) {
+            clientStatus = Client.CLIENT_NOT_EXIST;
+            return;
+        }
+
+        ArrayList<String> hash = new ArrayList<String>();
+        File f = null;
+
+        for (String name : files) {
+            f = new File(OSManager.getClientFolder(applicationName), name);
+            if (!f.exists()) {
+                clientStatus = Client.WRONG_CLIENT;
+                return;
+            }
+
+            hash.add(MD5.getMD5(f));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : hash) {
+            sb.append(s);
+        }
+
+        switch (send("standart", sb.toString(), OSManager.getPlatform())) {
+            case BAD_CONNECTION:
+                onFinish();
+                clientStatus = Client.BAD_CONNECTION;
+                return;
+
+            case CLIENT_DOES_NOT_MATCH:
+                onFinish();
+                clientStatus = Client.CLIENT_DOES_NOT_MATCH;
+                return;
+
+            case CLIENT_NOT_EXIST:
+                onFinish();
+                clientStatus = Client.CLIENT_NOT_EXIST;
+                return;
+
+            case CLIENT_NOT_EXIST_ON_SERVER:
+                onFinish();
+                clientStatus = Client.CLIENT_NOT_EXIST_ON_SERVER;
+                return;
+
+            case OK:
+                onFinish();
+                clientStatus = Client.OK;
+                return;
+
+            case WRONG_CLIENT:
+                onFinish();
+                clientStatus = Client.WRONG_CLIENT;
+                return;
+        }
+    }
+
+    Client getClientStatus() {
+        return clientStatus;
+    }
+
+    private static Client send(String client, String hash, OSManager.OS system) {
+        try {
+            StringBuilder data = new StringBuilder(URLEncoder.encode("action", "UTF-8") + "=" + URLEncoder.encode("check", "UTF-8"));
+
+            data.append("&" + URLEncoder.encode("client", "UTF-8") + "=" + URLEncoder.encode(client, "UTF-8"));
+            data.append("&" + URLEncoder.encode("hash", "UTF-8") + "=" + URLEncoder.encode(hash, "UTF-8"));
+            data.append("&" + URLEncoder.encode("system", "UTF-8") + "=" + URLEncoder.encode(system.toString(), "UTF-8"));
+
+            URL url;
+
+            if (!Engine.getDescriptionFile().get("folder").equalsIgnoreCase("")) {
+                url = new URL(
+                        "http://" + Engine.getDescriptionFile().get("domain") + "/" + Engine.getDescriptionFile().get("folder") + "/" + "engine.php"
+                );
+            } else {
+                url = new URL(
+                        "http://" + Engine.getDescriptionFile().get("domain") + "/" + "engine.php"
+                );
+            }
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+
+            writer.write(data.toString());
+            writer.flush();
+
+            StringBuffer s = new StringBuffer();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                s.append(line);
+            }
+            writer.close();
+            reader.close();
+
+            String answer = s.toString();
+
+            if (Client.valueOf(answer) != null) {
+                return Client.valueOf(answer);
+            } else {
+                return Client.BAD_CONNECTION;
+            }
+        } catch (IOException e) {
+            return Client.BAD_CONNECTION;
+        }
+    }
+
+    private static void onFinish() {
+        for (CheckerListener cl : listeners) {
+            cl.onFinishChecking();
+        }
+    }
+
 }
--- src/org/sinrel/engine/actions/Intent.java
+++ src/org/sinrel/engine/actions/Intent.java
@@ -1,5 +1,8 @@
 package org.sinrel.engine.actions;
 
+import org.sinrel.engine.Engine;
+import org.sinrel.engine.library.cryption.Base64;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,100 +11,92 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 
-import org.sinrel.engine.Engine;
-import org.sinrel.engine.library.cryption.Base64;
-
 public abstract class Intent {
-	
-	@SuppressWarnings("unused")
-	private static String session;
-	
-	public final static void Do( Action a ) {
-		switch ( a ) {
-			case ENABLE : 
-				Engine.getLauncher().onEnable();
-				break;
-			case DISABLE :
-				Engine.getLauncher().onDisable();
-				break;
-		}
-	}
-	
-	/**
-	 * Старт скачивания клиента "standart"
-	 * @param loadZip Загружать ли client.zip
-	 * @param dir Имя папки в которой находится клиент ( пример: minecraft )
-	 * @return Возвращает одно из значений Download
-	 */
-	public static final Download DoDownload( String dir  , boolean loadZip ) {
-		return new Downloader( dir , loadZip ).getAnswer();
-	}
-	
-	public static final Auth DoAuth( String login , String pass ) {
-	     try {	    	 
-	    	  StringBuilder data = new StringBuilder( URLEncoder.encode("action", "UTF-8") + "=" + URLEncoder.encode("auth", "UTF-8") );
-	 		  data.append( "&" + URLEncoder.encode("login", "UTF-8") + "=" + URLEncoder.encode( Base64.encode( Base64.encode( login ) ) , "UTF-8") );
-	 		  data.append( "&" + URLEncoder.encode("pass", "UTF-8") + "=" + URLEncoder.encode( Base64.encode( Base64.encode( pass ) ), "UTF-8") );
-	 		  
-	          URL url; 
-	          
-	          if( !Engine.getDescriptionFile().get("folder").equalsIgnoreCase("") ) {
-	        	  url =	new URL( 
-	        			  "http://" + Engine.getDescriptionFile().get("domain") + "/" +  Engine.getDescriptionFile().get("folder") + "/" + "engine.php" 
-	        	  );
-	          }else {
-	        	  url =	new URL( 
-	        			  "http://" + Engine.getDescriptionFile().get("domain") + "/" + "engine.php" 
-	        	  );
-	          }
-	          	          
-	          URLConnection conn = url.openConnection();
-	          conn.setDoOutput(true);
-	          OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
 
-	          writer.write( data.toString() );
-	          writer.flush();
-	          
-	          StringBuffer s = new StringBuffer();
-	          BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	          String line;
-	          
-	          while ((line = reader.readLine()) != null) {
-	              s.append(line);
-	          }
-	          writer.close();
-	          reader.close();
-	          
-	          String answer = s.toString();	  
-	          
-	          if( answer.contains("OK") ) {
-	        	  session = answer.split("<:>")[1];
-	        	  
-	        	  return Auth.OK;
-	          }
-	          
-	          switch ( answer ) {
-	           case "BAD_CONNECTION":
-	        	   return Auth.BAD_CONNECTION;
-	        	   
-	           case "LOGIN_OR_PASS_NOT_EXIST":
-	        	   return Auth.LOGIN_OR_PASS_NOT_EXIST;
-	        	   
-	           case "BAD_LOGIN_OR_PASSWORD":
-	        	   return Auth.BAD_LOGIN_OR_PASSWORD;
-	          }
-	     }catch (IOException e) {
-	 		return Auth.BAD_CONNECTION;
-	     }
-	     
-	     return Auth.BAD_CONNECTION;
-	}
-	
-	public static final Client DoCheckClient( String applicationName , String serverName ) {
-		return new Checker( applicationName , serverName ).getClientStatus();
-	}
-	
-	public static final Client DoCheckClient( String applicationName ) {
-		return new Checker( applicationName ).getClientStatus();
-	}
+    @SuppressWarnings("unused")
+    private static String session;
+
+    public final static void Do(Action a) {
+        switch (a) {
+            case ENABLE:
+                Engine.getLauncher().onEnable();
+                break;
+            case DISABLE:
+                Engine.getLauncher().onDisable();
+                break;
+        }
+    }
+
+    /**
+     * Старт скачивания клиента "standart"
+     *
+     * @param loadZip Загружать ли client.zip
+     * @param dir     Имя папки в которой находится клиент ( пример: minecraft )
+     * @return Возвращает одно из значений Download
+     */
+    public static final Download DoDownload(String dir, boolean loadZip) {
+        return new Downloader(dir, loadZip).getAnswer();
+    }
+
+    public static final Auth DoAuth(String login, String pass) {
+        try {
+            StringBuilder data = new StringBuilder(URLEncoder.encode("action", "UTF-8") + "=" + URLEncoder.encode("auth", "UTF-8"));
+            data.append("&" + URLEncoder.encode("login", "UTF-8") + "=" + URLEncoder.encode(Base64.encode(Base64.encode(login)), "UTF-8"));
+            data.append("&" + URLEncoder.encode("pass", "UTF-8") + "=" + URLEncoder.encode(Base64.encode(Base64.encode(pass)), "UTF-8"));
+
+            URL url;
+
+            if (!Engine.getDescriptionFile().get("folder").equalsIgnoreCase("")) {
+                url = new URL(
+                        "http://" + Engine.getDescriptionFile().get("domain") + "/" + Engine.getDescriptionFile().get("folder") + "/" + "engine.php"
+                );
+            } else {
+                url = new URL(
+                        "http://" + Engine.getDescriptionFile().get("domain") + "/" + "engine.php"
+                );
+            }
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+
+            writer.write(data.toString());
+            writer.flush();
+
+            StringBuffer s = new StringBuffer();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                s.append(line);
+            }
+            writer.close();
+            reader.close();
+
+            String answer = s.toString();
+
+            if (answer.contains("OK")) {
+                session = answer.split("<:>")[1];
+
+                return Auth.OK;
+            }
+
+            if (Auth.valueOf(answer) != null) {
+                return Auth.valueOf(answer);
+            } else {
+                return Auth.BAD_CONNECTION;
+            }
+
+        } catch (IOException e) {
+            return Auth.BAD_CONNECTION;
+        }
+    }
+
+    public static final Client DoCheckClient(String applicationName, String serverName) {
+        return new Checker(applicationName, serverName).getClientStatus();
+    }
+
+    public static final Client DoCheckClient(String applicationName) {
+        return new Checker(applicationName).getClientStatus();
+    }
 }
