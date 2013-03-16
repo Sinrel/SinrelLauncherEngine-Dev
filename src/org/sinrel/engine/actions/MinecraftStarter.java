a116dca25161db3a7c6ee420774a3900e05706e0
Akirus <AkirusG@Gmail.com>
2013-03-16 22:41:38 +0300

    clean up

--- src/org/sinrel/engine/actions/MinecraftStarter.java
+++ src/org/sinrel/engine/actions/MinecraftStarter.java
@@ -3,5 +3,5 @@ package org.sinrel.engine.actions;
 import java.awt.Frame;
 
 public interface MinecraftStarter {
-	abstract void startMinecraft(String dir, String login, String session, boolean useAutoConnect, String server, String port, Frame frame);
+	void startMinecraft(String dir, String login, String session, boolean useAutoConnect, String server, String port, Frame frame);
 }
